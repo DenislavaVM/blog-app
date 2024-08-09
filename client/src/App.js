d4433be1aa094ff255d12783954196d29790aff2
Deni <denislavavmilanova@outlook.com>
2024-08-09 10:18:25 +0300

    Update App.js with basic layout and add global styles

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <div>Test</div>
+    <main>
+    <header>
+      <a href="/" className="logo">Blog</a>
+      <nav>
+        <a href="/login">Login</a>
+        <a href="/register">Register</a>
+      </nav>
+    </header>
+  </main>
   );
 }
 
