ced56c307638477111a7ae0453ba85619571b442
Deni <denislavavmilanova@outlook.com>
2024-08-09 13:06:32 +0300

    Add MainLayout component to manage overall application layout

---
+++ client/src/layouts/MainLayout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function MainLayout({ children }) {
+  return (
+    <div>
+      <Header />
+      <main>
+        {children}
+      </main>
+      {/* For Footer or other layout components */}
+    </div>
+  );
+}
+
+export default MainLayout;
