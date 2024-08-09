e03891ba90a0cc83e809fa8ea37e1580d9a9b322
Deni <denislavavmilanova@outlook.com>
2024-08-09 13:07:05 +0300

    Create posts.js to centralize blog post data

---
+++ client/src/data/posts.js
@@ -0,0 +1,12 @@
+const posts = [
+    {
+      id: 1,
+      title: "The Resilient Beauty of Sunflowers",
+      author: "Me",
+      date: "2024-01-06 15:48",
+      description: "In a world of fleeting moments, the sunflower stands tall as a symbol of resilience and hope. Discover the fascinating journey of these golden blooms, from their origins in ancient cultures to their modern-day significance. Explore the deep connection between sunflowers and the human spirit, and how their bright petals inspire us to find strength and beauty in every season of life.",
+      imgSrc: "https://cdn.stocksnap.io/img-thumbs/960w/macro-yellow_SRX6B3DNYA.jpg"
+    },
+  ];
+  
+  export default posts;
