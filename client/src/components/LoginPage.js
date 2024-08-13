ac1d83f616de231d5f1b8b3706eebba5d9d27a36
Deni <denislavavmilanova@outlook.com>
2024-08-13 18:24:35 +0300

    Implement user login functionality on the frontend

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,14 +1,55 @@
-import React from 'react';
-import './AuthContainer.css';  // Import shared styles
+import React, { useState } from 'react';
+import './AuthContainer.css';
 
 function LoginPage() {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  async function login(ev) {
+    ev.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:5000/login", {
+        method: "POST",
+        body: JSON.stringify({ username, password }),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        alert("Login successful");
+        // Redirect to another page
+      } else {
+        alert("Login failed: " + data.error);
+      }
+    } catch (error) {
+      console.error("An error occurred during login:", error);
+      alert("An error occurred. Please try again.");
+    }
+  }
+
   return (
     <div className="auth-container">
       <h1>Sign In</h1>
-      <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Password" />
-      <a href="#">Forgot Your Password?</a>
-      <button type="button">Sign In</button>
+      <form onSubmit={login}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={username}
+          onChange={ev => setUsername(ev.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={ev => setPassword(ev.target.value)}
+        />
+        <a href="#">Forgot Your Password?</a>
+        <button type="submit">Sign In</button>
+      </form>
     </div>
   );
 }
