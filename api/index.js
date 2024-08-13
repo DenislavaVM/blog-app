fe61f78171b38de04b740ba258528d8e5a40608e
Deni <denislavavmilanova@outlook.com>
2024-08-13 18:25:06 +0300

    Set up Express server with user login routes

--- api/index.js
+++ api/index.js
@@ -16,28 +16,51 @@ mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.error("Failed to connect to MongoDB", err));
 
-    app.post("/register", async (req, res) => {
-        const { username, email, password } = req.body;
-        try {
-            const existingUser = await User.findOne({ email });
-            if (existingUser) {
-                return res.status(400).json({ error: "User already exists" });
-            }
-    
-            const hashedPassword = bcrypt.hashSync(password, salt);
-    
-            const userData = await User.create({
-                username,
-                email,
-                password: hashedPassword,
-            });
-    
-            res.json({ username, email });
-        } catch (error) {
-            console.error("Registration error:", error); // Log the error on the server
-            res.status(400).json({ error: "Registration failed" });
+app.post("/register", async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ error: "User already exists" });
         }
-    });
+
+        const hashedPassword = bcrypt.hashSync(password, salt);
+
+        const userData = await User.create({
+            username,
+            email,
+            password: hashedPassword,
+        });
+
+        res.json({ username, email });
+    } catch (error) {
+        console.error("Registration error:", error);
+        res.status(400).json({ error: "Registration failed" });
+    }
+});
+
+
+app.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const existingUser = await User.findOne({ username }); 
+        
+        if (!existingUser) {
+            return res.status(400).json({ error: "User not found" });
+        }
+
+        const isPasswordValid = bcrypt.compareSync(password, existingUser.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({ error: "Invalid password" });
+        }
+
+        res.json({ message: "Login successful", user: existingUser.username });
+    } catch (error) {
+        console.error("Login error:", error);
+        res.status(500).json({ error: "Login failed" });
+    }
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
