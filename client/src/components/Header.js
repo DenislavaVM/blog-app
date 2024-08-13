bc65554a6763303c6e7dc77c9911d3d134928aac
Deni <denislavavmilanova@outlook.com>
2024-08-13 19:43:10 +0300

    Add conditional rendering for "Create new post" and "Logout" links based on user login status in Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function Header() {
 
+  const [username, setUsername] = useState(null)
+
   useEffect(() => {
     fetch("http://localhost:5000/profile", {
       credentials: "include",
-    })
+    }).then(response => {
+      response.json().then(userInfo => {
+        setUsername(userInfo, username);
+      });
+    });
   }, []);
 
   return (
     <header>
       <Link to="/" className="logo">Blog</Link>
       <nav>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
+        {username && (
+          <>
+            <Link to="/create">Create new post</Link>
+            <a>Logout</a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
       </nav>
     </header>
   );
