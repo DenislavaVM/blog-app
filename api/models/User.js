2bc6d7259a239c045c97d5559d1400016af6c1af
Deni <denislavavmilanova@outlook.com>
2024-08-13 16:32:09 +0300

    Define Mongoose User schema with validation and constraints

---
+++ api/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const { model } = mongoose;
+
+const UserSchema = new mongoose.Schema({
+    username: { 
+        type: String, 
+        required: true, 
+        minlength: 4, 
+        unique: true,
+        trim: true
+    },
+    email: { 
+        type: String, 
+        required: true, 
+        minlength: 4, 
+        unique: true,
+        trim: true, 
+        match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address']
+    },
+    password: { 
+        type: String, 
+        required: true,
+        minlength: 6 
+    },
+});
+
+const UserModel = model("User", UserSchema);
+
+module.exports = UserModel;
+
