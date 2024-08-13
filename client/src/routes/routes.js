d5f270c7b779850a77bc34ef50996d1faee1e69a
Deni <denislavavmilanova@outlook.com>
2024-08-13 14:51:35 +0300

    Add routes for LoginPage and RegisterPage components

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from '../components/Home';
 import FullArticle from '../components/FullArticle';
+import LoginPage from '../components/LoginPage';
+import RegisterPage from '../components/RegisterPage';
 import posts from '../data/posts';
 
 const routes = (
   <>
     <Route path="/" element={<Home posts={posts} />} />
+    <Route path="/login" element={<LoginPage />} /> 
+    <Route path="/register" element={<RegisterPage />} /> 
     {posts.map(post => (
       <Route key={post.id} path={`/post/${post.id}`} element={<FullArticle post={post} />} />
     ))}
