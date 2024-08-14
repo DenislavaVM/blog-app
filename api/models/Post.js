a09d470aa5fd68b08b265bbaf5c8e5959e0491c7
Deni <denislavavmilanova@outlook.com>
2024-08-14 14:33:56 +0300

    Add Post model for storing post details in MongoDB

---
+++ api/models/Post.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    summary: { type: String, required: true },
+    content: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+});
+
+const PostModel = mongoose.model("Post", PostSchema);
+
+module.exports = PostModel;
